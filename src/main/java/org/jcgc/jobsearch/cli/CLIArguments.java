b05860c26c37e1bba4edbc5dc8e7c8cd1825cc4a
Juan Galvis <galvisjuanc@gmail.com>
2023-12-20 21:06:38 -0500

    CLI Arguments for API

---
+++ src/main/java/org/jcgc/jobsearch/cli/CLIArguments.java
@@ -0,0 +1,20 @@
+package org.jcgc.jobsearch.cli;
+
+public class CLIArguments {
+
+    CLIArguments() {
+
+    }
+
+    private String keyword;
+
+    private String location;
+
+    private int page = 0;
+
+    private boolean isFullTime = false;
+
+    private boolean isMarkdown = false;
+
+    private boolean isHelp;
+}
