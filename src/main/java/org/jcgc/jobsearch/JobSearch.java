e7d0eaeed810fff6f67f9b852068c4779d2f0f72
Juan Galvis <galvisjuanc@gmail.com>
2023-12-25 16:00:23 -0500

    Tweak 1

--- src/main/java/org/jcgc/jobsearch/JobSearch.java
+++ src/main/java/org/jcgc/jobsearch/JobSearch.java
@@ -18,7 +18,7 @@ import static org.jcgc.jobsearch.api.APIFunctions.buildAPI;
 
 public class JobSearch {
     public static void main(String[] args) {
-        System.out.println("Hello job search app");
+        System.out.println("Hello job search app -- Job");
         JCommander jCommander = buildCommanderWithName("job-search", CLIArguments::newInstance);
 
         Stream<CLIArguments> streamOfCLI =
